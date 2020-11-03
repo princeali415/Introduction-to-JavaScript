b5507614c6a3504da3d36b825bd073534a2b4db5
Hussain Ali <ali.415code@gmail.com>
2020-11-03 14:19:25 -0800

    updated js

--- index.js
+++ index.js
@@ -235,9 +235,15 @@ Using the annoyingSong function below do the following:
       "(number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
-  }
+function annoyingSong(num){
+  let str = "";
+  while (num > 0){
+      str = console.log(num + " bottles of soda on the wall, " + num + " bottles of soda, take one down pass it around " + (num = num-1) +" bottles of soda on the wall");
+  // eslint-disable-next-line no-extra-semi
+  };
+  return str;
+// eslint-disable-next-line no-extra-semi
+};
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
